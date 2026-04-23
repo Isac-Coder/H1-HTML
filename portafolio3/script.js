50fcdd66fec224056f3fa2548db0a6f994f19a7a
Isac <isaacalvarez0516@gmail.com>
2026-04-23 14:30:45 -0500

    add: script code

--- portafolio3/script.js
+++ portafolio3/script.js
@@ -1,6 +1,17 @@
-Document.eventListener("DOMContentLoaded", function() {
-    const botonEnviar = document.getElementById("butt-enviar");
-    botonEnviar.addEventListener("click", function(alert) {
-        alert("¡Mensaje enviado!");
+const formulario = document.getElementById('miFormulario');
+const boton = document.getElementById('botonEnviar');
+
+formulario.addEventListener('input', () => {
+    if (formulario.checkValidity()) {
+        boton.disabled = false;
+        boton.style.opacity = "1";
+    } else {
+        boton.disabled = true;
+        boton.style.opacity = "0.5";
+    }
     });
-});
+
+
+document.getElementById("botonEnviar").onclick = function() {
+    alert('¡Datos enviados!');
+};
