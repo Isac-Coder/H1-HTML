a00e1616a5a2a1c78248c3d5f832de65c11c6abb
Isac <isaacalvarez0516@gmail.com>
2026-04-24 12:32:38 -0500

    arreglo envio

--- portafolio3/script.js
+++ portafolio3/script.js
@@ -1,7 +1,11 @@
 const formulario = document.getElementById('miFormulario');
 const boton = document.getElementById('botonEnviar');
 
+
 formulario.addEventListener('input', () => {
+    document.getElementById("botonEnviar").onclick = function() {
+        alert('¡Datos enviados!');
+    };
     if (formulario.checkValidity()) {
         boton.disabled = false;
         boton.style.opacity = "1";
@@ -12,6 +16,3 @@ formulario.addEventListener('input', () => {
     });
 
 
-document.getElementById("botonEnviar").onclick = function() {
-    alert('¡Datos enviados!');
-};
